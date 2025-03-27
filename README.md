67a6bdcaff87e229c29f76f3b635695e93ca7875
deansg <deansg@gmail.com>
2025-03-27 14:14:52 +0200

    Few basic additions:
    1. Add some info to README.md
    2. Add argument parsing in main

---
+++ README.md
@@ -0,0 +1,15 @@
+# YouTube Playlist Exporter
+
+Tired of videos disappearing from your YouTube playlists, without leaving you the option to know what was there before?
+
+Fear not! For this CLI will have you covered
+
+ ![alt text](memes/1.jpg)
+
+## TODOs
+
+1. Finish initial implementation
+2. Export as proper package with installation etc.
+3. Export CSV with additional details (video ID, channel that uploaded etc.)
+4. Compare files based on video IDs?
+5. Add additional backup mechanisms? GIT backup?
--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from youtube-playlist-exporter!")
+import os
+
+import typer
+from typing_extensions import Annotated
+
+
+def main(playlist_id: Annotated[str, typer.Argument(help="The YouTube id of the playlist to be backed up")],
+         youtube_auth_key: Annotated[str, typer.Argument(
+             help="The API key provided by YouTube")],
+         output_dir: Annotated[str, typer.Argument(
+             default_factory=os.getcwd,
+             help="The script's output directory (must be an existing directory!) [default: current working directory]")],
+         playlist_name: Annotated[str | None, typer.Argument(
+             help="The name of the playlist to back-up. Only used for generating the names of the output files [default: the playlist id]")] = None,
+         are_new_videos_last: Annotated[bool, typer.Argument(
+             help="Whether new videos are added to the end of the playlist (in favorites' playlists they are added to the beginning, in other playlists to the end)")] = True):
+    typer.echo(f"Input is: "
+               f"playlist_id: {playlist_id}"
+               f"youtube_auth_key: {youtube_auth_key}"
+               f"output_dir: {output_dir}"
+               f"playlist_name: {playlist_name}"
+               f"are_new_videos_last: {are_new_videos_last}"
+               )
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
